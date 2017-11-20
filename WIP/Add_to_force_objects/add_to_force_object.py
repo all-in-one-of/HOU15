6ff081411bc1a0a61df8a6f8edc3110b4e2f0a85
Kishen PJ <33799510+kishenpj@users.noreply.github.com>
2017-11-19 22:19:38 -0500

    first_commit

---
+++ WIP/Add_to_force_objects/add_to_force_object.py
@@ -0,0 +1,26 @@
+import hou
+
+selection = hou.selectedNodes()
+names = [n.name() for n in selection]
+my_list = ""
+if len(selection) < 1 :
+    my_list = "nothing selected, please select at least one object"
+else:
+    a=1
+    for sel in selection:
+        my_list = my_list + "\n" + "selected object " + str(a) + ":" + sel.name()
+        a=a+1        
+hou.ui.displayMessage(my_list, buttons=('OK',))
+length = len(selection)
+print "Number of nodes selected: " + str(length)
+paths = [n.path() for n in hou.node('/out').allSubChildren()
+         if n.type() == hou.nodeType('Driver/ifd')]
+selected = hou.ui.selectFromTree(choices=paths)
+
+mantras = [hou.node(path) for path in selected]
+for m in mantras:
+    m.parm('forceobject').set(' '.join(n.name() for n in hou.selectedNodes())) # CHANGE PARAMETER NAME AS REQURIED
+    m.parm('vobject').set("")
+    print ("Selected mantra nodes : ") + m.name()
+    new_name=' '.join(names)
+hou.ui.displayMessage("( "+ (new_name) + " )  - added to selected mantra(s) matte objects.", buttons=('*Claps*',))
